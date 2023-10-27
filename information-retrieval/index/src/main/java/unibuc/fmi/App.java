8ce2d880eddf80973ec90d9bd2a6fcdcdc2e6aeb
fusedbloxxer <voidwarden@yahoo.com>
2023-10-27 22:31:58 +0300

    Allow submodule execution

--- information-retrieval/index/src/main/java/unibuc/fmi/App.java
+++ information-retrieval/index/src/main/java/unibuc/fmi/App.java
@@ -4,10 +4,8 @@ package unibuc.fmi;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! - Index");
     }
 }
