ad3f2c761c56f4123c030545f9d861a581f64f6d
fusedbloxxer <voidwarden@yahoo.com>
2023-10-28 22:30:47 +0300

    Update .pom files to include .java files in output

--- information-retrieval/index/src/main/java/unibuc/fmi/App.java
+++ information-retrieval/index/src/main/java/unibuc/fmi/App.java
@@ -1,12 +1,18 @@
 package unibuc.fmi;
 
-/**
- * Hello world!
- *
- */
+import org.apache.commons.collections.MapIterator;
+import org.apache.commons.collections.map.HashedMap;
+
 public class App {
     public static void main(String[] args) {
-        Useful.Hey();
-        System.out.println("Hello World! - Index");
+        System.out.println("Index.");
+        HashedMap map = new HashedMap();
+        map.put("andrei", "are");
+        MapIterator it = map.mapIterator();
+        while (it.hasNext()) {
+            Object key = it.next();
+            Object value = it.getValue();
+            System.out.println(key + " " + value);
+        }
     }
 }
