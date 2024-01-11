8d3bd623ccc9e83d4fc255e38eea7690a1c3d5c8
fusedbloxxer <voidwarden@yahoo.com>
2024-01-11 23:38:36 +0200

    Rework dataset part 1

--- emotion-analysis/emotion_analysis/data/dataset.py
+++ emotion-analysis/emotion_analysis/data/dataset.py
@@ -1,15 +1,23 @@
-import typing as t
+from typing import Any, TypedDict, List, Literal, Dict, cast
 from torch.utils.data import Dataset
+from collections import namedtuple
 from dataclasses import dataclass
 from functools import partial
+from pathlib import Path
 import pathlib as pb
 import pandas as pd
+from pandas import DataFrame
 import json
 import os
 import re
+import jax
+from jax import Array
+import jax.numpy as jnp
 
-
-Emotion: t.TypeAlias = t.Literal['neutral', 'anger', 'surprise', 'sadness', 'joy', 'disgust', 'fear']
+from .types import EmotionCauseConversation
+from. types import EmotionCauseEncoding
+from .types import EmotionCauseData
+from .types import Emotion
 
 
 @dataclass
@@ -42,8 +50,8 @@ class Utterance(object):
 @dataclass
 class Conversation(object):
     conversation_id: int
-    conversation: t.List[Utterance]
-    emotion_cause: t.List[t.List[str]]
+    conversation: List[Utterance]
+    emotion_cause: List[List[str]]
 
     @classmethod
     def from_dict(cls, data: dict, video_dir: pb.Path | None) -> 'Conversation':
@@ -57,86 +65,26 @@ class Conversation(object):
             conversation_id=data['conversation_ID'],
         )
 
-    @staticmethod
-    def to_df(conversations: t.List['Conversation'],
-              emotion_to_class: t.Dict[str, int],
-              with_text_range: bool,
-              with_pairs: bool) -> t.Tuple[pd.DataFrame, pd.DataFrame | None]:
-        # Transform to pandas format for faster processing
-        labels_: t.List[t.Dict[str, t.Any]] = []
-        data_: t.List[t.Dict[str, t.Any]] = []
-
-        # Aggregate all conversations in two separate DFs
-        for sample in conversations:
-            for utterance in sample.conversation:
-                # Obtain emotion
-                label_or_none: int | None = emotion_to_class.get(str(utterance.emotion), None)
-
-                # Add entry with ids starting from zero
-                data_.append({
-                    'conversation_id': sample.conversation_id - 1,
-                    'utterance_id': utterance.uterrance_id - 1,
-                    'speaker': utterance.speaker,
-                    'text': utterance.text,
-                    'emotion': label_or_none,
-                    'video_path': utterance.video_path,
-                })
-
-            if with_pairs:
-                for [emotion, cause] in sample.emotion_cause:
-                    # Obtain emotion
-                    utterance_id: int = int(emotion.split('_')[0]) - 1
-                    emotion_str: str = ''.join(emotion.split('_')[1:])
-                    label: int = emotion_to_class[emotion_str]
-
-                    # Obtain cause
-                    cause_id: int = int(cause.split('_')[0]) - 1
-
-                    # Find the indices of the cause for task 1 only
-                    if with_text_range:
-                        cause_str: str = ''.join(cause.split('_')[1:])
-                        cause_start = sample.conversation[cause_id].text.find(cause_str)
-                        cause_stop = cause_start + len(cause_str) - 1
-                        assert cause_start != -1, 'could not find cause range (conv_id: {}, utt_id: {}, cause_id: {}, cause_str: {})' \
-                            .format(sample.conversation_id, utterance_id, cause_id, cause_str)
-                    else:
-                        cause_start = None
-                        cause_stop = None
-
-                    # Add entry with ids starting from zero
-                    labels_.append({
-                        'conversation_id': sample.conversation_id - 1,
-                        'utterance_id': utterance_id,
-                        'emotion': label,
-                        'cause_id': cause_id,
-                        'cause_start': cause_start,
-                        'cause_stop': cause_stop, 
-                    })
-
-        labels = pd.DataFrame(labels_) if with_pairs else None
-        data = pd.DataFrame(data_)
-        return data, labels
-
 
 class ECACDataset(Dataset):
     def __init__(self,
                  data_dir: pb.Path,
-                 task: t.Literal['task_1', 'task_2'] = 'task_1',
-                 split: t.Literal['train', 'eval', 'test'] = 'train') -> None:
+                 task: Literal['task_1', 'task_2'] = 'task_1',
+                 split: Literal['train', 'eval', 'test'] = 'train') -> None:
         super().__init__()
         assert task in ('task_1', 'task_2'), 'invalid task type'
         assert split in ('train', 'eval', 'test'), 'invalid split type'
 
         # Data paths
-        self.data_dir_path = data_dir / 'competition' / split / task
-        self.metadata_path = self.data_dir_path / f'{task}_{split}.json'
-        self.video_dir_path = self.data_dir_path / 'video'
         self.video_pattern: str = r'dia\d+utt\d+\.mp4'
+        self.data_dir_path = data_dir / split / task
+        self.data_path = self.data_dir_path / f'{task}_{split}.json'
+        self.video_dir_path = self.data_dir_path / 'video'
 
-        # Dataset config
-        self.task: t.Literal['task_1', 'task_2'] = task
-        self.split: t.Literal['train', 'eval', 'test'] = split
-        self.emotions = ['neutral', 'anger', 'surprise', 'sadness', 'joy', 'disgust', 'fear']
+        # Dataset settings
+        self.task: Literal['task_1', 'task_2'] = task
+        self.split: Literal['train', 'eval', 'test'] = split
+        self.emotions: List[str] = ['neutral', 'anger', 'surprise', 'sadness', 'joy', 'disgust', 'fear']
         self.emotion_to_class = {e: i for i, e in enumerate(self.emotions)}
         self.class_to_emotion = {i: e for i, e in enumerate(self.emotions)}
         self.num_emotions = len(self.emotions)
@@ -144,8 +92,8 @@ class ECACDataset(Dataset):
         # Integrity checks
         if not self.data_dir_path.exists():
             raise FileNotFoundError(self.data_dir_path)
-        if not self.metadata_path.exists():
-            raise FileNotFoundError(self.metadata_path)
+        if not self.data_path.exists():
+            raise FileNotFoundError(self.data_path)
         if self.task == 'task_2':
             if not self.video_dir_path.exists():
                 raise FileNotFoundError(self.video_dir_path)
@@ -154,30 +102,94 @@ class ECACDataset(Dataset):
             if not all(map(lambda x: re.fullmatch(self.video_pattern, x.name), video_paths)):
                 raise Exception('found invalid files under ' + str(self.video_dir_path))
 
-        # Read metadata in-memory
-        with open(self.metadata_path, 'r') as file:
-            # Json -> Conversation DataClass
-            make_conversation = partial(Conversation.from_dict, video_dir=None if self.task == 'task_1' else self.video_dir_path)
-            metadata = list(map(make_conversation, json.load(file)))
+        # Read and prepare the dataset
+        data = self.__read_data(self.data_path)
+        data = self.__transform_data(data)
+        self.data = data
+        # self.data = self.__tokenize_data(self.data)
 
-            # Conversation DataClass -> DataFrame
-            self.data_, self.labels_ = Conversation.to_df(
-                emotion_to_class=self.emotion_to_class,
-                with_text_range=self.task == 'task_1',
-                with_pairs=self.split == 'train',
-                conversations=metadata,
-            )
+    # def __getitem__(self, index: int):
+    #     data = self.data_[self.data_['conversation_id'] == index]
+    #     if self.split == 'train':
+    #         assert self.labels_ is not None, 'no labels were found for train split'
+    #         labels = self.labels_[self.labels_['conversation_id'] == index]
+    #     else:
+    #         labels = None
+    #     return (data, labels)
 
-    def __getitem__(self, index: int):
-        data = self.data_[self.data_['conversation_id'] == index]
+    # def __len__(self) -> int:
+    #     return len(self.data_['conversation_id'].unique())
 
-        if self.split == 'train':
-            assert self.labels_ is not None, 'no labels were found for train split'
-            labels = self.labels_[self.labels_['conversation_id'] == index]
-        else:
-            labels = None
+    def __read_data(self, path: Path) -> List[EmotionCauseConversation]:
+        with open(path, 'r') as dataset_file:
+            return json.load(dataset_file)
 
-        return (data, labels)
+    def __transform_data(self, dataset: List[EmotionCauseConversation]) -> EmotionCauseData:
+        # Transform to pandas format for faster processing
+        data_, labels_ = [], []
 
-    def __len__(self) -> int:
-        return len(self.data_['conversation_id'].unique())
+        # Aggregate all conversations in two separate DFs
+        for conversation in dataset:
+            for utterance in conversation['conversation']:
+                # First add data that is always present
+                data_.append({
+                    'conversation_id': conversation['conversation_ID'] - 1,
+                    'utterance_id': utterance['utterance_ID'] - 1,
+                    'speaker': utterance['speaker'],
+                    'text': utterance['text'],
+                })
+                
+                # Obtain utterance emotion
+                if 'emotion' in utterance:
+                    emotion: str = cast(str, utterance.get('emotion', ''))
+                    emotion_label: int | None = self.emotion_to_class.get(emotion, None)
+                    data_[-1]['emotion'] = emotion_label
+
+                # Obtain video_path 
+                if self.task == 'task_2':
+                    video_name = f'dia{conversation["conversation_ID"]}utt{utterance["utterance_ID"]}.mp4'
+                    video_path = self.video_dir_path / video_name
+                    assert os.path.exists(video_path), f'no video file was found on disk at {video_path}'
+                    data_[-1]['video_path'] = video_path
+
+            # Proceed further only if labels are available
+            if self.split != 'train':
+                continue
+            if not ('emotion-cause_pairs' in conversation and conversation['emotion-cause_pairs']):
+                continue
+
+            # Retireve all causal labels
+            for emotion, cause in conversation['emotion-cause_pairs']:
+                # First add data that is always present
+                labels_.append({
+                    'conversation_id': conversation['conversation_ID'] - 1,
+                })
+
+                # Obtain emotion
+                utterance_id: int = int(emotion.split('_')[0]) - 1
+                emotion_str: str = ''.join(emotion.split('_')[1:])
+                label: int = self.emotion_to_class[emotion_str]
+                labels_[-1]['utterance_id'] = utterance_id
+                labels_[-1]['emotion'] = label
+
+                # Obtain cause
+                cause_id: int = int(cause.split('_')[0]) - 1
+                labels_[-1]['cause_id'] = cause_id
+
+                # Find the indices of the cause for task 1 only
+                if self.task == 'task_1':
+                    cause_str: str = ''.join(cause.split('_')[1:])
+                    cause_index_start: int = conversation['conversation'][cause_id]['text'].find(cause_str)
+                    cause_index_stop: int = cause_index_start + len(cause_str) - 1
+                    assert cause_index_start != -1, 'could not find cause range (conv_id: {}, utt_id: {}, cause_id: {}, cause_str: {})' \
+                        .format(conversation['conversation_ID'], utterance_id, cause_id, cause_str)
+                    labels_[-1]['cause_start'] = cause_index_start
+                    labels_[-1]['cause_stop'] = cause_index_stop
+
+        # Aggregate data and labels
+        data: DataFrame = pd.DataFrame(data_)
+        labels: DataFrame | None = pd.DataFrame(labels_) if self.split == 'train' else None
+        return EmotionCauseData(data, labels)
+
+    def __tokenize_data(self, dataset: EmotionCauseData) -> EmotionCauseEncoding:
+        pass
---
+++ emotion-analysis/emotion_analysis/data/types.py
@@ -0,0 +1,56 @@
+from jax import Array
+from typing import TypedDict, Optional, List, Tuple, Literal, TypeAlias
+from pandas import DataFrame
+from dataclasses import dataclass
+
+
+# Paul Ekman Basic Emotion Categories
+Emotion: TypeAlias = Literal[
+    'neutral',
+    'anger',
+    'surprise',
+    'sadness',
+    'joy',
+    'disgust',
+    'fear'
+]
+
+
+# Types from JSON File
+EmotionCauseUtterance = TypedDict('EmotionCauseUtterance', {
+    'utterance_ID': int,
+    'text': str,
+    'speaker': str,
+    'emotion': Optional[str],
+    'video_name': Optional[str],
+})
+EmotionCauseConversation = TypedDict('EmotionCauseConversation', {
+    'conversation_ID': int,
+    'conversation': List[EmotionCauseUtterance],
+    'emotion-cause_pairs': Optional[List[Tuple[str, str]]]
+})
+
+
+# Intermediary Dataset representation
+@dataclass
+class EmotionCauseData(object):
+    # Data
+    conversation: DataFrame
+
+    # Labels
+    emotion_cause_pairs: Optional[DataFrame] = None
+
+
+# Final Dataset Representation
+class EmotionCauseEncoding(TypedDict):
+    # Data
+    input_ids: Array
+    input_mask: Array
+    attention_mask: Array
+    offset_mapping: Array
+
+    # Labels
+    cause_mask: Optional[Array]
+    cause_span: Optional[Array]
+    cause_labels: Optional[Array]
+    emotion_labels: Optional[Array]
